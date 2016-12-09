e26fe2b90a97ffe0dd34033173b40923596cf813
Gil Carmel <gil.carmel@gmail.com>
2016-12-09 08:51:03 -0800

    more throttle

--- drive.py
+++ drive.py
@@ -38,7 +38,7 @@ def telemetry(sid, data):
     transformed_image_array = image_array[None, :, :, :]
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
+    throttle = 0.4
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
